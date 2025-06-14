ab70b9b73ccb7569e7a07b30916690f4c998f7d3
DrZiMo <zuhaybhamar@gmail.com>
2025-06-14 17:26:38 +0300

    added the error.js and also loading.js and so on

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   devIndicators: false,
+  images: {
+    remotePatterns: [
+      new URL(
+        'https://pzyiopjmenomcskpiogi.supabase.co/storage/v1/object/public/cabin-images/**'
+      ),
+    ],
+  },
 }
 
 export default nextConfig
