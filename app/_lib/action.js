06474929f84f11052c4a5f093c7c6f2e8a11e863
DrZiMo <zuhaybhamar@gmail.com>
2025-06-17 17:21:57 +0300

    feat: enhance DateSelector and ReservationForm with booking logic, add createBooking function, and update styles

--- app/_lib/action.js
+++ app/_lib/action.js
@@ -98,3 +98,29 @@ export async function UpdateBooking(formData) {
   revalidatePath(`/account/reservations/edit/${id}`)
   redirect('/account/reservations')
 }
+
+export async function createBooking(bookingData, formData) {
+  const session = await auth()
+
+  if (!session) throw new Error('You must be logged in')
+
+  const newBooking = {
+    ...bookingData,
+    guestId: session.user.guestId,
+    numGuests: Number(formData.get('numGuests')),
+    observations: formData.get('observations').slice(0, 1000),
+    extraPrice: 0,
+    totalPrice: bookingData.cabinPrice,
+    isPaid: false,
+    hasBreakfast: false,
+    status: 'unconfirmed',
+  }
+
+  console.log(newBooking)
+
+  const { error } = await supabase.from('bookings').insert([newBooking])
+
+  if (error) {
+    throw new Error('Booking could not be created')
+  }
+}
